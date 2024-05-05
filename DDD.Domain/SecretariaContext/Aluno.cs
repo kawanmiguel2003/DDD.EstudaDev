cb813a24a566cb286bf7887f293b9dd064aa36ba
kawanmiguel2003 <miguel.kawan2003@gmail.com>
2024-05-05 13:35:07 -0300

    Adicionar arquivos de projeto.

---
+++ DDD.Domain/SecretariaContext/Aluno.cs
@@ -0,0 +1,27 @@
+using DDD.Domain.BibliotecaContext;
+using DDD.Domain.UserManagementContext;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDD.Domain.SecretariaContext
+{
+    public class Aluno : User
+    {
+        public IList<Disciplina>? Disciplinas { get; set; }
+
+        public IList<Livro>? Livros { get; set; }
+
+
+
+
+
+
+        //public IList<Matricula>? Matriculas { get; set; }
+
+    }
+}
